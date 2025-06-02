b1ed3a848355a8833bf3d5b5d941caa011545c66
Eivind <60471005+Vespertine13@users.noreply.github.com>
2025-06-02 16:00:19 +0000

    test: add test to second script

--- second/tests/second_test.py
+++ second/tests/second_test.py
@@ -1,4 +1,5 @@
 from second.second import greeting
+import pytest
 
 def test_greeting_eng():
     assert greeting("James", "English") == "Hello James!"
@@ -15,3 +16,16 @@ def test_greeting_default():
 # continue by using assert
 # add your changes: git add second/tests/second_test.py
 # commit your changes using commit message conventions (https://inpred.github.io/24-03_bioinfo_ws/#19): git commit -m "test: <your commit message>"
+
+def test_numeric_name():
+    with pytest.raises(TypeError):
+        assert greeting(2,"somelanguage") == None
+
+def test_numeric_language():
+    with pytest.raises(TypeError):
+        assert greeting("somename",2) == None
+
+def test_bool_language():
+    with pytest.raises(TypeError):
+        assert greeting("somename",True) == None
+
